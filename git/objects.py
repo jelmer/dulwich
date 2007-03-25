7cf5612d20d88841860f8445fb308ed5e8faccb0
James Westby <jw+debian@jameswestby.net>
2007-03-25 13:28:11 +0000

    Make it more like a real project.
    Add copyright statements, and license the code under the GPLv2.
    
    Also add a README file.

--- git/objects.py
+++ git/objects.py
@@ -1,3 +1,24 @@
+# objects.py -- Acces to base git objects
+# Copyright (C) 2007 James Westby <jw+debian@jameswestby.net>
+# The header parsing code is based on that from git itself, which is
+# Copyright (C) 2005 Linus Torvalds
+# and licensed under v2 of the GPL.
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
 import mmap
 import os
 import sha

c4c19475f32c7eb7a195c2134f30f81aa279f4cc
James Westby <jw+debian@jameswestby.net>
2007-03-25 13:28:11 +0000

    Make it more like a real project.
    Add copyright statements, and license the code under the GPLv2.
    
    Also add a README file.

--- git/tests/__init__.py
+++ git/tests/__init__.py
@@ -1,3 +1,21 @@
+# __init__.py -- The tests for python-git
+# Copyright (C) 2007 James Westby <jw+debian@jameswestby.net>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
 import unittest
 import test_objects
 

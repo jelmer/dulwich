0f1c1d9f97a3283b83611380ba77d22eecadf74c
Jelmer Vernooij <jelmer@samba.org>
2008-12-11 16:52:29 +0000

    Add iterentries().

--- dulwich/tests/test_pack.py
+++ dulwich/tests/test_pack.py
@@ -110,6 +110,10 @@ class TestPackData(PackTests):
     p = self.get_pack_data(pack1_sha)
     self.assertEquals([(12, 1, 'tree b2a2766a2879c209ab1176e7e778b81ae422eeaa\nauthor James Westby <jw+debian@jameswestby.net> 1174945067 +0100\ncommitter James Westby <jw+debian@jameswestby.net> 1174945067 +0100\n\nTest commit\n'), (138, 2, '100644 a\x00og\x0c\x0f\xb5?\x94cv\x0br\x95\xfb\xb8\x14\xe9e\xfb \xc8'), (178, 3, 'test 1\n')], list(p.iterobjects()))
 
+  def test_iterentries(self):
+    p = self.get_pack_data(pack1_sha)
+    self.assertEquals([('og\x0c\x0f\xb5?\x94cv\x0br\x95\xfb\xb8\x14\xe9e\xfb \xc8', 178, -1718046665), ('\xb2\xa2vj(y\xc2\t\xab\x11v\xe7\xe7x\xb8\x1a\xe4"\xee\xaa', 138, -901046474), ('\xf1\x8f\xaa\x16S\x1a\xc5p\xa3\xfd\xc8\xc7\xca\x16h%H\xda\xfd\x12', 12, 1185722901)], list(p.iterentries()))
+
   def test_create_index_v1(self):
     p = self.get_pack_data(pack1_sha)
     p.create_index_v1("v1test.idx")

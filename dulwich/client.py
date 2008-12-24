a68843fcb9df0f64b722fcd0de208d009a40043b
John Carr <john.carr@unrouted.co.uk>
2008-12-24 22:47:35 +0000

    Factor out common code

--- dulwich/client.py
+++ dulwich/client.py
@@ -18,7 +18,7 @@
 
 import select
 import socket
-
+from dulwich.protocol import Protocol
 
 def extract_capabilities(text):
     if not "\0" in text:
@@ -58,41 +58,9 @@ class GitClient(object):
     """
 
     def __init__(self, fileno, read, write, host):
-        self.read = read
-        self.write = write
+        self.proto = Protocol(read, write)
         self.fileno = fileno
         self.host = host
-
-    def read_pkt_line(self):
-        """
-        Reads a 'pkt line' from the remote git process
-
-        :return: The next string from the stream
-        """
-        sizestr = self.read(4)
-        if sizestr == "":
-            raise RuntimeError("Socket broken")
-        size = int(sizestr, 16)
-        if size == 0:
-            return None
-        return self.read(size-4)
-
-    def read_pkt_seq(self):
-        pkt = self.read_pkt_line()
-        while pkt:
-            yield pkt
-            pkt = self.read_pkt_line()
-
-    def write_pkt_line(self, line):
-        """
-        Sends a 'pkt line' to the remote git process
-
-        :param line: A string containing the data to send
-        """
-        if line is None:
-            self.write("0000")
-        else:
-            self.write("%04x%s" % (len(line)+4, line))
 
     def send_cmd(self, name, *args):
         self.write_pkt_line("%s %s" % (name, "".join(["%s\0" % a for a in args])))
@@ -104,7 +72,7 @@ class GitClient(object):
         server_capabilities = None
         refs = {}
         # Receive refs from server
-        for pkt in self.read_pkt_seq():
+        for pkt in self.proto.read_pkt_seq():
             (sha, ref) = pkt.rstrip("\n").split(" ", 1)
             if server_capabilities is None:
                 (ref, server_capabilities) = extract_capabilities(ref)
@@ -117,12 +85,12 @@ class GitClient(object):
         refs, server_capabilities = self.read_refs()
         changed_refs = [] # FIXME
         if not changed_refs:
-            self.write_pkt_line(None)
+            self.proto.write_pkt_line(None)
             return
-        self.write_pkt_line("%s %s %s\0%s" % (changed_refs[0][0], changed_refs[0][1], changed_refs[0][2], self.capabilities()))
+        self.proto.write_pkt_line("%s %s %s\0%s" % (changed_refs[0][0], changed_refs[0][1], changed_refs[0][2], self.capabilities()))
         for changed_ref in changed_refs[:]:
-            self.write_pkt_line("%s %s %s" % changed_refs)
-        self.write_pkt_line(None)
+            self.proto.write_pkt_line("%s %s %s" % changed_refs)
+        self.proto.write_pkt_line(None)
         # FIXME: Send pack
 
     def fetch_pack(self, path, determine_wants, graph_walker, pack_data, progress):
@@ -139,32 +107,32 @@ class GitClient(object):
        
         wants = determine_wants(refs)
         if not wants:
-            self.write_pkt_line(None)
+            self.proto.write_pkt_line(None)
             return
-        self.write_pkt_line("want %s %s\n" % (wants[0], self.capabilities()))
+        self.proto.write_pkt_line("want %s %s\n" % (wants[0], self.capabilities()))
         for want in wants[1:]:
-            self.write_pkt_line("want %s\n" % want)
-        self.write_pkt_line(None)
+            self.proto.write_pkt_line("want %s\n" % want)
+        self.proto.write_pkt_line(None)
         have = graph_walker.next()
         while have:
-            self.write_pkt_line("have %s\n" % have)
+            self.proto.write_pkt_line("have %s\n" % have)
             if len(select.select([self.fileno], [], [], 0)[0]) > 0:
-                pkt = self.read_pkt_line()
+                pkt = self.proto.read_pkt_line()
                 parts = pkt.rstrip("\n").split(" ")
                 if parts[0] == "ACK":
                     graph_walker.ack(parts[1])
                     assert parts[2] == "continue"
             have = graph_walker.next()
-        self.write_pkt_line("done\n")
-        pkt = self.read_pkt_line()
+        self.proto.write_pkt_line("done\n")
+        pkt = self.proto.read_pkt_line()
         while pkt:
             parts = pkt.rstrip("\n").split(" ")
             if parts[0] == "ACK":
                 graph_walker.ack(pkt.split(" ")[1])
             if len(parts) < 3 or parts[2] != "continue":
                 break
-            pkt = self.read_pkt_line()
-        for pkt in self.read_pkt_seq():
+            pkt = self.proto.read_pkt_line()
+        for pkt in self.proto.read_pkt_seq():
             channel = ord(pkt[0])
             pkt = pkt[1:]
             if channel == 1:

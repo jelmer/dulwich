6108cbd2da2fe36c524b0ccda2102dd0f4c1b3dd
Jelmer Vernooij <jelmer@samba.org>
2009-03-21 22:59:20 +0000

    Use stored checksum rather than re-calculating.

--- dulwich/object_store.py
+++ dulwich/object_store.py
@@ -140,7 +140,7 @@ class ObjectStore(object):
         entries = p.sorted_entries()
         basename = os.path.join(self.pack_dir(), 
             "pack-%s" % iter_sha1(entry[0] for entry in entries))
-        write_pack_index_v2(basename+".idx", entries, p.calculate_checksum())
+        write_pack_index_v2(basename+".idx", entries, p.get_stored_checksum())
         os.rename(path, basename + ".pack")
 
     def add_thin_pack(self):

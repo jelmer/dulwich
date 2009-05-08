1b0d5648f2d7df97525cf1bffe067bbc01ac5ce8
Jelmer Vernooij <jelmer@samba.org>
2009-05-08 20:51:45 +0200

    Add stub for commit, extra check.

--- dulwich/tests/test_objects.py
+++ dulwich/tests/test_objects.py
@@ -63,6 +63,7 @@ class BlobReadTests(unittest.TestCase):
         sha = 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391'
         b = self.get_blob(sha)
         self.assertEqual(b.data, '')
+        self.assertEqual(b.id, sha)
         self.assertEqual(b.sha().hexdigest(), sha)
   
     def test_create_blob_from_string(self):

f1262e1fcc35d2a80caa56db01bd70a554439ac2
Jelmer Vernooij <jelmer@samba.org>
2009-05-08 19:01:25 +0200

    More docstrings.

--- dulwich/object_store.py
+++ dulwich/object_store.py
@@ -16,6 +16,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301, USA.
 
+
+"""Git object store interfaces and implementation."""
+
+
 import itertools
 import os
 import tempfile

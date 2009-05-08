7efd347407e369ffdde3f1945d0329832d0d9e43
Jelmer Vernooij <jelmer@samba.org>
2009-05-08 23:01:56 +0200

    More commit_tree tests and fixes.

--- dulwich/tests/test_index.py
+++ dulwich/tests/test_index.py
@@ -84,5 +84,19 @@ class CommitTreeTests(TestCase):
         blobs = [("bla", blob.id, stat.S_IFREG)]
         rootid = commit_tree(self.store, blobs)
         self.assertEquals(rootid, "1a1e80437220f9312e855c37ac4398b68e5c1d50")
-        self.assertEquals(blob.id, self.store[rootid]["bla"][1])
+        self.assertEquals((stat.S_IFREG, blob.id), self.store[rootid]["bla"])
         self.assertEquals(set([rootid, blob.id]), set(self.store._data.keys()))
+
+    def test_nested(self):
+        blob = Blob()
+        blob.data = "foo"
+        self.store.add_object(blob)
+        blobs = [("bla/bar", blob.id, stat.S_IFREG)]
+        rootid = commit_tree(self.store, blobs)
+        self.assertEquals(rootid, "d92b959b216ad0d044671981196781b3258fa537")
+        dirid = self.store[rootid]["bla"][1]
+        self.assertEquals(dirid, "c1a1deb9788150829579a8b4efa6311e7b638650")
+        self.assertEquals((stat.S_IFDIR, dirid), self.store[rootid]["bla"])
+        self.assertEquals((stat.S_IFREG, blob.id), self.store[dirid]["bar"])
+        self.assertEquals(set([rootid, dirid, blob.id]), 
+                          set(self.store._data.keys()))

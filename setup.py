2e636c64d28233decb023f86bcf88108609fe206
Jelmer Vernooij <jelmer@samba.org>
2009-05-10 16:04:37 +0200

    Release 0.3.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-dulwich_version_string = '0.2.2'
+dulwich_version_string = '0.3.0'
 
 setup(name='dulwich',
       description='Pure-Python Git Library',

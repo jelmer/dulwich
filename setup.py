ae1b509f727e3ca4d403447f31be5cc0a69542b3
Jelmer Vernooij <jelmer@samba.org>
2009-10-07 11:29:12 +0200

    Start working on 0.4.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-dulwich_version_string = '0.4.0'
+dulwich_version_string = '0.4.1'
 
 include_dirs = []
 # Windows MSVC support

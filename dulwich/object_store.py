130a62d76e56da3dcc450cfd78a20d56b764566b
Jelmer Vernooij <jelmer@samba.org>
2009-11-28 20:20:10 +0100

    Remove trailing whitespace.

--- dulwich/object_store.py
+++ dulwich/object_store.py
@@ -1,16 +1,16 @@
-# object_store.py -- Object store for git objects 
+# object_store.py -- Object store for git objects
 # Copyright (C) 2008-2009 Jelmer Vernooij <jelmer@samba.org>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # or (at your option) a later version of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -40,8 +40,8 @@ from dulwich.objects import (
     )
 from dulwich.pack import (
     Pack,
-    PackData, 
-    iter_sha1, 
+    PackData,
+    iter_sha1,
     load_pack_index,
     write_pack,
     write_pack_data,
@@ -71,7 +71,7 @@ class BaseObjectStore(object):
 
     def get_raw(self, name):
         """Obtain the raw text for an object.
-        
+
         :param name: sha for the object.
         :return: tuple with object type and object contents.
         """

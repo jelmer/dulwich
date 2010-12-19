dac48b6f4ca8f3431c6484385fb81cbe5ce8219a
Jelmer Vernooij <jelmer@samba.org>
2010-12-19 20:48:27 +0100

    dulwich.tests.compat: Fix rest syntax problems in module docstrings.

--- dulwich/tests/compat/test_server.py
+++ dulwich/tests/compat/test_server.py
@@ -20,8 +20,8 @@
 """Compatibility tests between Dulwich and the cgit server.
 
 Warning: these tests should be fairly stable, but when writing/debugging new
-tests, deadlocks may freeze the test process such that it cannot be Ctrl-C'ed.
-On *nix, you can kill the tests with Ctrl-Z, "kill %".
+    tests, deadlocks may freeze the test process such that it cannot be
+    Ctrl-C'ed. On POSIX systems, you can kill the tests with Ctrl-Z, "kill %".
 """
 
 import threading

bd2ec45f5fa2d456632fe8d006fbafaa2b502e4c
Jelmer Vernooij <jelmer@samba.org>
2011-03-26 16:45:33 +0100

    Fix syntax in Pack.keep docstring.

--- dulwich/pack.py
+++ dulwich/pack.py
@@ -1500,9 +1500,11 @@ class Pack(object):
 
     def keep(self, msg=None):
         """Add a .keep file for the pack, preventing git from garbage collecting it.
-           :param msg: A message written inside the .keep file; can be used later to
-                       determine whether or not a .keep file is obsolete.
-           :return: The path of the .keep file, as a string."""
+
+        :param msg: A message written inside the .keep file; can be used later to
+                    determine whether or not a .keep file is obsolete.
+        :return: The path of the .keep file, as a string.
+        """
         keepfile_name = '%s.keep' % self._basename
         keepfile = GitFile(keepfile_name, 'wb')
         try:

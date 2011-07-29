87c62e7eb342f515ef13e014217fe40fe5a33de4
Dave Borowitz <dborowitz@google.com>
2011-07-29 15:58:19 +0200

    walk: Raise MissingCommitError instead of KeyError.
    Change-Id: I675597821a9eae6055240b76893c53756fd11215

--- dulwich/errors.py
+++ dulwich/errors.py
@@ -83,6 +83,7 @@ class MissingCommitError(Exception):
     """Indicates that a commit was not found in the repository"""
 
     def __init__(self, sha, *args, **kwargs):
+        self.sha = sha
         Exception.__init__(self, "%s is not in the revision store" % sha)
 
 

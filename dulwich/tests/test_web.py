03192bae4fecdde15d03b0a4e79cbccd731ecbff
Dave Borowitz <dborowitz@google.com>
2011-08-01 20:42:16 +0200

    Pass an HTTP request object through to handlers.
    Replaces the boolean stateless_rpc argument. This allows handlers to
    not only detect whether they're running in a stateless RPC call, but
    also to use request-specific data.
    
    Change-Id: I19623fda358c6806ac759a61a183ed561a357a86

--- dulwich/tests/test_web.py
+++ dulwich/tests/test_web.py
@@ -282,11 +282,11 @@ class DumbHandlersTestCase(WebTestCase):
 class SmartHandlersTestCase(WebTestCase):
 
     class _TestUploadPackHandler(object):
-        def __init__(self, backend, args, proto, stateless_rpc=False,
+        def __init__(self, backend, args, proto, http_req=None,
                      advertise_refs=False):
             self.args = args
             self.proto = proto
-            self.stateless_rpc = stateless_rpc
+            self.http_req = http_req
             self.advertise_refs = advertise_refs
 
         def handle(self):
@@ -318,7 +318,7 @@ class SmartHandlersTestCase(WebTestCase):
         self.assertEqual('handled input: foo', write_output)
         self.assertContentTypeEquals('application/x-git-upload-pack-response')
         self.assertFalse(self._handler.advertise_refs)
-        self.assertTrue(self._handler.stateless_rpc)
+        self.assertTrue(self._handler.http_req)
         self.assertFalse(self._req.cached)
 
     def test_handle_service_request(self):
@@ -350,7 +350,7 @@ class SmartHandlersTestCase(WebTestCase):
         # Ensure all output was written via the write callback.
         self.assertEquals('', handler_output)
         self.assertTrue(self._handler.advertise_refs)
-        self.assertTrue(self._handler.stateless_rpc)
+        self.assertTrue(self._handler.http_req)
         self.assertFalse(self._req.cached)
 
 

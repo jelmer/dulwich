604498805d78fa41ba3ed9e2f9090afba2f658c9
Dave Borowitz <dborowitz@google.com>
2011-08-01 20:42:16 +0200

    Pass an HTTP request object through to handlers.
    Replaces the boolean stateless_rpc argument. This allows handlers to
    not only detect whether they're running in a stateless RPC call, but
    also to use request-specific data.
    
    Change-Id: I19623fda358c6806ac759a61a183ed561a357a86

--- dulwich/web.py
+++ dulwich/web.py
@@ -169,7 +169,7 @@ def get_info_refs(req, backend, mat):
         write = req.respond(HTTP_OK, 'application/x-%s-advertisement' % service)
         proto = ReceivableProtocol(StringIO().read, write)
         handler = handler_cls(backend, [url_prefix(mat)], proto,
-                              stateless_rpc=True, advertise_refs=True)
+                              http_req=req, advertise_refs=True)
         handler.proto.write_pkt_line('# service=%s\n' % service)
         handler.proto.write_pkt_line(None)
         handler.handle()
@@ -246,7 +246,7 @@ def handle_service_request(req, backend, mat):
     if content_length:
         input = _LengthLimitedFile(input, int(content_length))
     proto = ReceivableProtocol(input.read, write)
-    handler = handler_cls(backend, [url_prefix(mat)], proto, stateless_rpc=True)
+    handler = handler_cls(backend, [url_prefix(mat)], proto, http_req=req)
     handler.handle()
 
 

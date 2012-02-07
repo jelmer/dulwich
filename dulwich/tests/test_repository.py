680e4d43f300cb6f7cc634773566db1c8f4db814
Risto Kankkunen <risto.kankkunen@iki.fi>
2012-02-07 22:08:16 +0200

    Don't look up "HOME" environment variable unconditionally to avoid KeyError if it's not defined, like on Windows which usually doesn't have "HOME", but "HOMEDRIVE" and "HOMEPATH" instead.

--- dulwich/tests/test_repository.py
+++ dulwich/tests/test_repository.py
@@ -320,8 +320,7 @@ class RepositoryTests(TestCase):
         self.assertIsInstance(r.get_config(), Config)
 
     def test_get_config_stack(self):
-        self.addCleanup(os.environ.__setitem__, "HOME", os.environ["HOME"])
-        os.environ["HOME"] = "/nonexistant"
+        self.makeSafeEnv()
         r = self._repo = open_repo('ooo_merge.git')
         self.assertIsInstance(r.get_config_stack(), Config)
 
@@ -460,8 +459,7 @@ class BuildRepoTests(TestCase):
         self.assertEquals("iso8859-1", r[commit_sha].encoding)
 
     def test_commit_config_identity(self):
-        self.addCleanup(os.environ.__setitem__, "HOME", os.environ["HOME"])
-        os.environ["HOME"] = "/nonexistant"
+        self.makeSafeEnv()
         # commit falls back to the users' identity if it wasn't specified
         r = self._repo
         c = r.get_config()

3b02a912ad6452742d2cfa2b414c189c12e2b86a
Andi McClure <andi.m.mcclure@gmail.com>
2012-07-13 13:51:15 +0200

    Take copy of refs when invoking determine_wants callback.
    This avoids breakage if the callback happens to change the value that was
    passed in.
    
    Signed-Off-By: Jelmer Vernooij <jelmer@samba.org>

--- dulwich/client.py
+++ dulwich/client.py
@@ -440,7 +440,7 @@ class TraditionalGitClient(GitClient):
         old_refs, server_capabilities = self._read_refs(proto)
         negotiated_capabilities = self._send_capabilities & server_capabilities
         try:
-            new_refs = determine_wants(old_refs)
+            new_refs = determine_wants(dict(old_refs))
         except:
             proto.write_pkt_line(None)
             raise
@@ -707,7 +707,7 @@ class HttpGitClient(GitClient):
         old_refs, server_capabilities = self._discover_references(
             "git-receive-pack", url)
         negotiated_capabilities = self._send_capabilities & server_capabilities
-        new_refs = determine_wants(old_refs)
+        new_refs = determine_wants(dict(old_refs))
         if new_refs is None:
             return old_refs
         if self.dumb:

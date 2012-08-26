e15f8632f9e9192d2b4c17a0de3ae0e2c75cb086
Jelmer Vernooij <jelmer@samba.org>
2012-08-26 21:25:56 +0200

    Add function for testing whether to ignore specific error.

--- dulwich/tests/compat/server_utils.py
+++ dulwich/tests/compat/server_utils.py
@@ -19,7 +19,7 @@
 
 """Utilities for testing git server compatibility."""
 
-
+import errno
 import os
 import select
 import shutil
@@ -200,3 +200,10 @@ class NoSideBand64kReceivePackHandler(ReceivePackHandler):
     def capabilities(cls):
         return tuple(c for c in ReceivePackHandler.capabilities()
                      if c != 'side-band-64k')
+
+
+def ignore_error((e_type, e_value, e_tb)):
+    """Check whether this error is safe to ignore."""
+    return (issubclass(e_type, socket.error) and
+            e_value[0] in (errno.ECONNRESET, errno.EPIPE))
+

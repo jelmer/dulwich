6d7efd15f4fc5862754d89a2592d90e050d42b88
Jelmer Vernooij <jelmer@samba.org>
2013-11-04 11:01:36 +0000

    Use write_info_refs.

--- dulwich/swift.py
+++ dulwich/swift.py
@@ -64,6 +64,7 @@ from dulwich.object_store import (
     )
 from dulwich.refs import (
     read_info_refs,
+    write_info_refs,
     )
 
 try:
@@ -138,18 +139,6 @@ def swift_load_pack_index(scon, filename):
         return load_pack_index_file(filename, f)
     finally:
         f.close()
-
-
-def put_info_refs(scon, filename, refs):
-    """Write info/refs file to Swift
-
-    :param scon: a `SwiftConnector` instance
-    :param filename: Path to the index file object
-    """
-    f = StringIO()
-    for refname, sha in refs.iteritems():
-        f.write("%s\t%s\n" % (sha, refname))
-    scon.put_object(filename, f)
 
 
 class SwiftException(Exception):
@@ -685,9 +674,10 @@ class SwiftInfoRefsContainer(InfoRefsContainer):
     """Manage references in info/refs object.
     """
 
-    def __init__(self, scon):
+    def __init__(self, scon, store):
         self.scon = scon
         self.filename = 'info/refs'
+        self.store = store
         f = self.scon.get_object(self.filename)
         if not f:
             f = StringIO('')
@@ -704,6 +694,11 @@ class SwiftInfoRefsContainer(InfoRefsContainer):
                 return False
         return refs
 
+    def _write_refs(self, refs):
+        f = StringIO()
+        f.writelines(write_info_refs(refs, self.store))
+        self.scon.put_object(self.filename, f)
+
     def set_if_equals(self, name, old_ref, new_ref):
         """Set a refname to new_ref only if it currently equals old_ref.
         """
@@ -711,7 +706,7 @@ class SwiftInfoRefsContainer(InfoRefsContainer):
         if not isinstance(refs, dict):
             return False
         refs[name] = new_ref
-        put_info_refs(self.scon, self.filename, refs)
+        self._write_refs(refs)
         self._refs[name] = new_ref
         return True
 
@@ -722,7 +717,7 @@ class SwiftInfoRefsContainer(InfoRefsContainer):
         if not isinstance(refs, dict):
             return False
         del refs[name]
-        put_info_refs(self.scon, self.filename, refs)
+        self._write_refs(refs)
         del self._refs[name]
         return True
 
@@ -751,7 +746,7 @@ class SwiftRepo(BaseRepo):
         self.bare = True
         self._controldir = self.root
         object_store = SwiftObjectStore(self.scon)
-        refs = SwiftInfoRefsContainer(self.scon)
+        refs = SwiftInfoRefsContainer(self.scon, object_store)
         BaseRepo.__init__(self, object_store, refs)
 
     def _put_named_file(self, filename, contents):
@@ -773,8 +768,7 @@ class SwiftRepo(BaseRepo):
         :return: a `SwiftRepo` instance
         """
         scon.create_root()
-        for obj in [posixpath.join(OBJECTDIR, PACKDIR),
-                    posixpath.join(INFODIR, 'refs')]:
+        for obj in [posixpath.join(OBJECTDIR, PACKDIR)]:
             scon.put_object(obj, StringIO(''))
         ret = cls(scon.root, conf)
         ret._init_files(True)

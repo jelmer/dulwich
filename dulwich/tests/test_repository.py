d47e9900e7c91d2e71cf332d0be0fda09f43fdba
rfaulk <rfaulk@yahoo-inc.com>
2014-01-19 17:44:43 -0800

    add - stage has default behaviour that stages all eligible files under root + simple test.

--- dulwich/tests/test_repository.py
+++ dulwich/tests/test_repository.py
@@ -677,3 +677,8 @@ class BuildRepoTests(TestCase):
         r.stage(['a'])
         r.stage(['a'])  # double-stage a deleted path
 
+    def test_stage_default_simple(self):
+        r = self._repo
+        r.stage()
+
+

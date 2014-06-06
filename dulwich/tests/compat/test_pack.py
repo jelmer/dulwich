0703edd76d9bb7e603a684a8ece950acfba430b5
Jelmer Vernooĳ <jelmer@samba.org>
2014-06-06 23:50:58 +0200

    For the moment, keep deltification disabled - but add a flag to enable it.

--- dulwich/tests/compat/test_pack.py
+++ dulwich/tests/compat/test_pack.py
@@ -80,7 +80,7 @@ class TestPack(PackTests):
         new_blob.data = orig_blob.data + 'x'
         all_to_pack = list(orig_pack.pack_tuples()) + [(new_blob, None)]
         pack_path = os.path.join(self._tempdir, "pack_with_deltas")
-        write_pack(pack_path, all_to_pack)
+        write_pack(pack_path, all_to_pack, deltify=True)
         output = run_git_or_fail(['verify-pack', '-v', pack_path])
         self.assertEqual(set(x[0].id for x in all_to_pack),
                          _git_verify_pack_object_list(output))

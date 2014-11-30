cb9badab82ca1ea584bbdd871c93b1f6bef7226b
Jelmer Vernooij <jelmer@samba.org>
2014-11-30 17:59:14 +0000

    Add pypy package.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class DulwichDistribution(Distribution):
             return True
 
     def has_ext_modules(self):
-        return not self.pure and not '__pypy__' in sys.modules
+        return not self.pure
 
     global_options = Distribution.global_options + [
         ('pure', None, "use pure Python code instead of C "

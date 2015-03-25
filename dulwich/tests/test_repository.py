f4e78d320c4977597095e47790d340e4a151b729
Jelmer Vernooij <jelmer@samba.org>
2015-03-25 01:36:12 +0000

    Release 0.10.1.

--- dulwich/tests/test_repository.py
+++ dulwich/tests/test_repository.py
@@ -122,7 +122,7 @@ class RepositoryTests(TestCase):
             self.assertEqual(k in r, contained)
 
         for k, _ in test_keys:
-            self.assertRaisesRegex(
+            self.assertRaisesRegexp(
                 TypeError, "'name' must be bytestring, not unicode",
                 r.__getitem__, unicode(k)
             )

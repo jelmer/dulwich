46166aa3d074f732b8b0e3e5c31f61a94df41d44
Jelmer Vernooij <jelmer@samba.org>
2015-03-25 01:37:10 +0000

    Start on 0.10.2.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup, Extension
 from distutils.core import Distribution
 
-dulwich_version_string = '0.10.1'
+dulwich_version_string = '0.10.2'
 
 include_dirs = []
 # Windows MSVC support

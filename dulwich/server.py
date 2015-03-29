e51e9bb3d9d8e56383b34d20854010312aba1f5e
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-03-29 16:47:37 +1300

    Not using property as per upstream requirement.

--- dulwich/server.py
+++ dulwich/server.py
@@ -242,7 +242,6 @@ class UploadPackHandler(Handler):
         # data (such as side-band, see the progress method here).
         self._processing_have_lines = False
 
-    @property
     def _done_required(self):
         return not self.has_capability("no-done")
 
@@ -317,7 +316,7 @@ class UploadPackHandler(Handler):
         self._processing_have_lines = False
 
         if not graph_walker.handle_done(
-                self._done_required, self._done_received):
+                self._done_required(), self._done_received):
             return
 
         self.progress("dul-daemon says what\n")

a107499dbac411a8078e89e70745d8421acb2256
Jelmer Vernooij <jelmer@samba.org>
2015-04-26 17:41:17 +0000

    Support Python3 in write_commit_patch.

--- dulwich/tests/test_patch.py
+++ dulwich/tests/test_patch.py
@@ -45,35 +45,34 @@ from dulwich.tests.utils import (
     )
 
 
-@skipIfPY3
 class WriteCommitPatchTests(TestCase):
 
-    def test_simple(self):
+    def test_simple_bytesio(self):
         f = BytesIO()
         c = Commit()
-        c.committer = c.author = "Jelmer <jelmer@samba.org>"
+        c.committer = c.author = b"Jelmer <jelmer@samba.org>"
         c.commit_time = c.author_time = 1271350201
         c.commit_timezone = c.author_timezone = 0
-        c.message = "This is the first line\nAnd this is the second line.\n"
+        c.message = b"This is the first line\nAnd this is the second line.\n"
         c.tree = Tree().id
         write_commit_patch(f, c, "CONTENTS", (1, 1), version="custom")
         f.seek(0)
         lines = f.readlines()
-        self.assertTrue(lines[0].startswith("From 0b0d34d1b5b596c928adc9a727a4b9e03d025298"))
-        self.assertEqual(lines[1], "From: Jelmer <jelmer@samba.org>\n")
-        self.assertTrue(lines[2].startswith("Date: "))
+        self.assertTrue(lines[0].startswith(b"From 0b0d34d1b5b596c928adc9a727a4b9e03d025298"))
+        self.assertEqual(lines[1], b"From: Jelmer <jelmer@samba.org>\n")
+        self.assertTrue(lines[2].startswith(b"Date: "))
         self.assertEqual([
-            "Subject: [PATCH 1/1] This is the first line\n",
-            "And this is the second line.\n",
-            "\n",
-            "\n",
-            "---\n"], lines[3:8])
+            b"Subject: [PATCH 1/1] This is the first line\n",
+            b"And this is the second line.\n",
+            b"\n",
+            b"\n",
+            b"---\n"], lines[3:8])
         self.assertEqual([
-            "CONTENTS-- \n",
-            "custom\n"], lines[-2:])
+            b"CONTENTS-- \n",
+            b"custom\n"], lines[-2:])
         if len(lines) >= 12:
             # diffstat may not be present
-            self.assertEqual(lines[8], " 0 files changed\n")
+            self.assertEqual(lines[8], b" 0 files changed\n")
 
 
 @skipIfPY3

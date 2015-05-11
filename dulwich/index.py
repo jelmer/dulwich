09c957e688c7b13d5172faed55e5b155fa307175
Gary van der Merwe <garyvdm@gmail.com>
2015-05-11 09:40:27 +0200

    In index.build_index_from_tree, pass the validate_path_element onto validate_path.

--- dulwich/index.py
+++ dulwich/index.py
@@ -484,7 +484,7 @@ def build_index_from_tree(prefix, index_path, object_store, tree_id,
     index = Index(index_path)
 
     for entry in object_store.iter_tree_contents(tree_id):
-        if not validate_path(entry.path):
+        if not validate_path(entry.path, validate_path_element):
             continue
         full_path = os.path.join(prefix, entry.path.decode(sys.getfilesystemencoding()))
 

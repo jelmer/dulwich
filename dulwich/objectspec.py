0c2125f689a741f58bcc7b02283f0c14ce81fa18
Chris Bunney <crbunney@users.noreply.github.com>
2015-11-30 20:40:27 +0000

    Enhanced parsing of partial/ambiguous refs based on https://git-scm.com/docs/gitrevisions

--- dulwich/objectspec.py
+++ dulwich/objectspec.py
@@ -46,7 +46,15 @@ def parse_ref(container, refspec):
     :raise KeyError: If the ref can not be found
     """
     refspec = to_bytes(refspec)
-    for ref in [refspec, b"refs/heads/" + refspec]:
+    possible_refs = [
+        refspec,
+        b"refs/" + refspec,
+        b"refs/tags/" + refspec,
+        b"refs/heads/" + refspec,
+        b"refs/remotes/" + refspec,
+        b"refs/remotes/"+refspec+b"/HEAD"
+    ]
+    for ref in possible_refs:
         if ref in container:
             return ref
     else:

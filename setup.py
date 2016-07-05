59c02ca11c67f989992c37b084b408b4c072d0f0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2016-07-05 22:58:12 +0000

    RElease 0.14.1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup, Extension
 from distutils.core import Distribution
 
-dulwich_version_string = '0.15.0'
+dulwich_version_string = '0.14.1'
 
 include_dirs = []
 # Windows MSVC support

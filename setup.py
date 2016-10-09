096fa9ce3d6ae274fa48179474c6c50a33d03657
Jelmer Vernooĳ <jelmer@jelmer.uk>
2016-10-09 13:24:12 +0000

    Start on 0.15.1.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from distutils.core import setup, Extension
 from distutils.core import Distribution
 
-dulwich_version_string = '0.15.0'
+dulwich_version_string = '0.15.1'
 
 include_dirs = []
 # Windows MSVC support

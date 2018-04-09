746656af64518e6ac420f9acd9f62b213b930cdc
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-04-09 03:25:13 +0100

    Strip forward slashes after conversion from os.path.sep.

--- dulwich/refs.py
+++ dulwich/refs.py
@@ -487,9 +487,10 @@ class DiskRefsContainer(RefsContainer):
         subkeys = set()
         path = self.refpath(base).encode(sys.getfilesystemencoding())
         for root, unused_dirs, files in os.walk(path):
-            dir = root[len(path):].strip(b'/')
+            dir = root[len(path):]
             if os.path.sep != '/':
                 dir = dir.replace(os.path.sep, "/")
+            dir = dir.strip(b'/')
             for filename in files:
                 refname = b"/".join(([dir] if dir else []) + [filename])
                 # check_ref_format requires at least one /, so we prepend the

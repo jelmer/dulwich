17d990542eb83db05aca76cc369ff4af6fcfd7ae
KS Chan <mrkschan@gmail.com>
2018-12-13 22:33:24 +0800

    Handle invalid ref that pretends to be a sub-folder under a valid ref

--- dulwich/refs.py
+++ dulwich/refs.py
@@ -617,7 +617,7 @@ class DiskRefsContainer(RefsContainer):
                     # Read only the first 40 bytes
                     return header + f.read(40 - len(SYMREF))
         except IOError as e:
-            if e.errno in (errno.ENOENT, errno.EISDIR):
+            if e.errno in (errno.ENOENT, errno.EISDIR, errno.ENOTDIR):
                 return None
             raise
 

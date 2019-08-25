dd07fa946b200c3b1cd692a64992bcbae7e20a54
monnerat <monnerat@users.noreply.github.com>
2019-08-26 00:47:38 +0200

    Allow configured email addresses to be pre-quoted as "<...>". (#722)
    Allow configured email addresses to be pre-quoted as "<...>".

--- dulwich/repo.py
+++ dulwich/repo.py
@@ -34,6 +34,7 @@ import os
 import sys
 import stat
 import time
+import re
 
 from dulwich.errors import (
     NoIndexPresent,
@@ -112,6 +113,8 @@ BASE_DIRECTORIES = [
 
 DEFAULT_REF = b'refs/heads/master'
 
+quoted_email_re = re.compile(br"^\s*<\s*(.*)\s*>\s*$")
+
 
 class InvalidUserIdentity(Exception):
     """User identity is not of the format 'user <email>'"""
@@ -175,6 +178,9 @@ def get_user_identity(config, kind=None):
         email = default_email
         if not isinstance(email, bytes):
             email = email.encode('utf-8')
+    m = quoted_email_re.match(email)
+    if m:
+        email = m.group(1)
     return (user + b" <" + email + b">")
 
 

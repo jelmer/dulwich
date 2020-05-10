5ea8a268201386356dad29d5827cc22dfdf82ec2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-10 20:11:08 +0000

    Actually include the python-patch license, since 'MIT License' is not necessarily clear.

--- dulwich/contrib/diffstat.py
+++ dulwich/contrib/diffstat.py
@@ -7,16 +7,31 @@
 #
 # This diffstat code was extracted and heavily modified from:
 #
-#    https://github.com/techtonik/python-patch
-#        Under the following license:
+#  https://github.com/techtonik/python-patch
+#      Under the following license:
 #
-#    Patch utility to apply unified diffs
-#    Brute-force line-by-line non-recursive parsing
+#  Patch utility to apply unified diffs
+#  Brute-force line-by-line non-recursive parsing
 #
-#    Copyright (c) 2008-2016 anatoly techtonik
-#    Available under the terms of MIT license
+# Copyright (c) 2008-2016 anatoly techtonik
 #
-# and falls under the exact same MIT license
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import sys
 import re

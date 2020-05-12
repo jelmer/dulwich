57df0b8ef3c98557cb05ed4da7a0373f1eed4129
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-12 01:59:48 +0000

    Update license merge_base module to the common Dulwich license.
    Permission from Kevin here: https://github.com/dulwich/dulwich/issues/754#issuecomment-626669534

--- dulwich/merge_base.py
+++ dulwich/merge_base.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
-"""
-Implementation of merge-base following the approach of git
+"""Implementation of merge-base following the approach of git
 """
 # Copyright (c) 2020 Kevin B. Hendricks, Stratford Ontario Canada
 #
-# Available under the MIT License
+# Dulwich is dual-licensed under the Apache License, Version 2.0 and the GNU
+# General Public License as public by the Free Software Foundation; version 2.0
+# or (at your option) any later version. You can redistribute it and/or
+# modify it under the terms of either of these two licenses.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You should have received a copy of the licenses; if not, see
+# <http://www.gnu.org/licenses/> for a copy of the GNU General Public License
+# and <http://www.apache.org/licenses/LICENSE-2.0> for a copy of the Apache
+# License, Version 2.0.
 
 from collections import deque
 

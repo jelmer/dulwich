32276c81de939070320e8102e244d2e5e1a1a6bd
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-22 16:29:37 +0000

    Cope with urlparse being imported as a function.

--- dulwich/client.py
+++ dulwich/client.py
@@ -1883,7 +1883,7 @@ def get_credentials_from_store(scheme, hostname, username=None,
         try:
             with open(fname, 'rb') as f:
                 for line in f:
-                    parsed_line = urlparse.urlparse(line)
+                    parsed_line = urlparse(line)
                     if (parsed_line.scheme == scheme and
                             parsed_line.hostname == hostname and
                             (username is None or

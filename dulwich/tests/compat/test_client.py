09cd5ea17e0c4ebeab0e71c61b889ecb9b995cec
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-06-06 21:30:05 +0000

    Fix updating of refs over HTTP(S) when there are no new objects to be sent. Fixes #739

--- dulwich/tests/compat/test_client.py
+++ dulwich/tests/compat/test_client.py
@@ -311,6 +311,20 @@ class DulwichClientTestBase(object):
                 self._build_path('/dest'), lambda _: sendrefs, gen_pack)
             self.assertFalse(b"refs/heads/abranch" in dest.refs)
 
+    def test_send_new_branch_empty_pack(self):
+        with repo.Repo(os.path.join(self.gitroot, 'dest')) as dest:
+            dummy_commit = self.make_dummy_commit(dest)
+            dest.refs[b'refs/heads/master'] = dummy_commit
+            dest.refs[b'refs/heads/abranch'] = dummy_commit
+            sendrefs = {b'refs/heads/bbranch': dummy_commit}
+            def gen_pack(have, want, ofs_delta=False):
+                return 0, []
+            c = self._client()
+            self.assertEqual(dest.refs[b"refs/heads/abranch"], dummy_commit)
+            c.send_pack(
+                self._build_path('/dest'), lambda _: sendrefs, gen_pack)
+            self.assertEqual(dummy_commit, dest.refs[b"refs/heads/abranch"])
+
     def test_get_refs(self):
         c = self._client()
         refs = c.get_refs(self._build_path('/server_new.export'))

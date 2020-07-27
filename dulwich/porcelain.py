22b54b560befe97612935ed86c350399d551db0f
Boris Feld <lothiraldan@gmail.com>
2020-07-27 16:22:03 +0200

    Cope with abspath/Path.resolve differences

--- dulwich/porcelain.py
+++ dulwich/porcelain.py
@@ -230,7 +230,14 @@ def path_to_tree_path(repopath, path, tree_encoding=DEFAULT_ENCODING):
         # https://bugs.python.org/issue38671
         if sys.platform == 'win32':
             path = Path(os.path.abspath(path))
+
         repopath = Path(repopath).resolve()
+        # Resolve and abspath seems to behave differently regarding symlinks, as
+        # we are doing abspath on the file path, we need to do the same on the
+        # repo path or they might not match
+        if sys.platform == 'win32':
+            repopath = Path(os.path.abspath(repopath))
+
         relpath = path.relative_to(repopath)
         if sys.platform == 'win32':
             return str(relpath).replace(os.path.sep, '/').encode(tree_encoding)

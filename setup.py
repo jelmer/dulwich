7c31a6419746c29c8519f3f2ee8421b217adafd9
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-10-30 13:26:13 +0000

    Release 0.20.10

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.version_info < (3, 5):
         'For 2.7 support, please install a version prior to 0.20')
 
 
-dulwich_version_string = '0.20.9'
+dulwich_version_string = '0.20.10'
 
 
 class DulwichDistribution(Distribution):

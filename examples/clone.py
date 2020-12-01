aa6f2c5def929f373ea82e55bb52230a75786fde
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-12-01 22:25:49 +0800

    Lint, style and added features
    Follow flake8, pylint, pymy and other lint, style python code.
    
    Issue 811

--- examples/clone.py
+++ examples/clone.py
@@ -3,8 +3,9 @@
 This trivial script demonstrates how to clone or lock a remote repository.
 
 Example usage:
-  1. python examples/clone.py git://github.com/jelmer/dulwich
-  2. python examples/clone.py git://github.com/jelmer/dulwich.git dulwich
+    1. python examples/clone.py git://github.com/jelmer/dulwich
+    2. python examples/clone.py git://github.com/jelmer/dulwich.git
+    3. python examples/clone.py git://github.com/jelmer/dulwich.git dulwich
 """
 
 
@@ -27,7 +28,10 @@ if len(args) < 2:
 elif len(args) < 3:
     target_path = basename(args[1].split(":")[-1])
     if target_path[-4:] == ".git":
-        target_path = target_path[:-4]
+        porcelain.clone(args[1], target_path[:-4])
+    else:
+        porcelain.clone(args[1], target_path)
+
 else:
     target_path = args[2]
 

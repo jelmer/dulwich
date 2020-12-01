285a0c98ea949775fea1e30583aebc935713be20
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-12-01 22:25:49 +0800

    Lint, style and added features
    Follow flake8, pylint, pymy and other lint, style python code.
    
    Issue 811

--- examples/clone.py
+++ examples/clone.py
@@ -1,18 +1,37 @@
-#!/usr/bin/python
-# This trivial script demonstrates how to clone a remote repository.
-#
-# Example usage:
-#  python examples/clone.py git://github.com/jelmer/dulwich dulwich-clone
+"""Clone.
+
+This trivial script demonstrates how to clone a remote repository.
+
+Example usage:
+    2. python examples/clone.py git://github.com/jelmer/dulwich.git dulwich
+    1. python examples/clone.py git://github.com/jelmer/dulwich.git
+"""
+
 
 import sys
+
+from os.path import basename
+
 from getopt import getopt
+
 from dulwich import porcelain
 
-opts, args = getopt(sys.argv, "", [])
-opts = dict(opts)
+
+_, args = getopt(sys.argv, "", [])
+
 
 if len(args) < 2:
     print("usage: %s host:path path" % (args[0], ))
     sys.exit(1)
 
-porcelain.clone(args[1], args[2])
+elif len(args) < 3:
+    repository = basename(args[1].split(":")[-1])
+    if repository[-4:] == ".git":
+        args.append(repository[:-4])
+    else:
+        args.append(repository)
+
+    porcelain.clone(args[1], args[2])
+
+else:
+    porcelain.clone(args[1], args[2])

6c4a222f51382313ed16aba65db1da5ba138bafa
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-12-16 22:36:09 +0800

    Delete redundant variable usage, increase usage method and adjust number
    pull 323

--- examples/clone.py
+++ examples/clone.py
@@ -1,10 +1,11 @@
 """Clone.
 
-This trivial script demonstrates how to clone a remote repository.
+This trivial script demonstrates how to clone or lock a remote repository.
 
 Example usage:
-    2. python examples/clone.py git://github.com/jelmer/dulwich.git dulwich
-    1. python examples/clone.py git://github.com/jelmer/dulwich.git
+    1. python examples/clone.py git://github.com/jelmer/dulwich
+    2. python examples/clone.py git://github.com/jelmer/dulwich.git
+    3. python examples/clone.py git://github.com/jelmer/dulwich.git dulwich
 """
 
 
@@ -27,11 +28,9 @@ if len(args) < 2:
 elif len(args) < 3:
     repository = basename(args[1].split(":")[-1])
     if repository[-4:] == ".git":
-        args.append(repository[:-4])
+        porcelain.clone(args[1], repository[:-4])
     else:
-        args.append(repository)
-
-    porcelain.clone(args[1], args[2])
+        porcelain.clone(args[1], repository)
 
 else:
     porcelain.clone(args[1], args[2])

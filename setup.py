1c35c60ad7320a448373355432014971b76a82a4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-04 12:48:11 +0000

    Release 0.20.17.

--- setup.py
+++ setup.py
@@ -23,8 +23,7 @@ if sys.version_info < (3, 5):
         'For 2.7 support, please install a version prior to 0.20')
 
 
-dulwich_version_string = '0.20.15'
-
+dulwich_version_string = '0.20.17'
 
 class DulwichDistribution(Distribution):
 

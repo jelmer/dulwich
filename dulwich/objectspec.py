3f1eeb4c7f90306d85f79dfef98fe31cca84668b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-03 17:57:50 +0000

    Add typing.

--- dulwich/objectspec.py
+++ dulwich/objectspec.py
@@ -20,6 +20,8 @@
 
 """Object specification."""
 
+from typing import Union, List, Tuple
+
 
 def to_bytes(text):
     if getattr(text, "encode", None) is not None:
@@ -118,7 +120,10 @@ def parse_reftuple(lh_container, rh_container, refspec, force=False):
     return (lh, rh, force)
 
 
-def parse_reftuples(lh_container, rh_container, refspecs, force=False):
+def parse_reftuples(
+        lh_container, rh_container,
+        refspecs: Union[bytes, List[bytes], List[Tuple[bytes, bytes]]],
+        force: bool = False):
     """Parse a list of reftuple specs to a list of reftuples.
 
     Args:

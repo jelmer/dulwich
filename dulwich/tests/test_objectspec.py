6062fc5d8298eee57f3fbaddaa3f80a3ef223bb8
Jelmer Vernooij <jelmer@jelmer.uk>
2021-10-03 12:05:57 +0200

    Add a test.

--- dulwich/tests/test_objectspec.py
+++ dulwich/tests/test_objectspec.py
@@ -258,3 +258,9 @@ class ParseTreeTests(TestCase):
         c1, c2, c3 = build_commit_graph(r.object_store, [[1], [2, 1], [3, 1, 2]])
         self.assertEqual(r[c1.tree], parse_tree(r, c1.id))
         self.assertEqual(r[c1.tree], parse_tree(r, c1.tree))
+
+    def test_from_ref(self):
+        r = MemoryRepo()
+        c1, c2, c3 = build_commit_graph(r.object_store, [[1], [2, 1], [3, 1, 2]])
+        r.refs[b'refs/heads/foo'] = c1.id
+        self.assertEqual(r[c1.tree], parse_tree(r, b'foo'))

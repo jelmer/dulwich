jelmer/dulwich

commit e9faf83ac437d59bea8ef82d14c87c147e4a508a

Chunk uploads.

dulwich/client.py
@@ -1970,8 +1970,9 @@ class AbstractHttpGitClient(GitClient):
         headers = {
             "Content-Type": "application/x-%s-request" % service,
             "Accept": result_content_type,
-            "Content-Length": str(len(data)),
         }
+        if isinstance(data, bytes):
+            headers["Content-Length"] = str(len(data))
         resp, read = self._http_request(url, headers, data)
         if resp.content_type != result_content_type:
             raise GitProtocolError(
@@ -2033,7 +2034,7 @@ class AbstractHttpGitClient(GitClient):
                 yield from PackChunkGenerator(pack_data_count, pack_data)
 
         resp, read = self._smart_request(
-            "git-receive-pack", url, data=b"".join(body_generator())
+            "git-receive-pack", url, data=body_generator()
         )
         try:
             resp_proto = Protocol(read, None)

dulwich/tests/compat/test_client.py
@@ -602,9 +602,23 @@ class GitHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         try:
             nbytes = int(length)
         except (TypeError, ValueError):
-            nbytes = 0
-        if self.command.lower() == "post" and nbytes > 0:
-            data = self.rfile.read(nbytes)
+            nbytes = -1
+        if self.command.lower() == "post":
+            if nbytes > 0:
+                data = self.rfile.read(nbytes)
+            elif self.headers.get('transfer-encoding') == 'chunked':
+                chunks = []
+                while True:
+                    line = self.rfile.readline()
+                    length = int(line.rstrip(), 16)
+                    chunk = self.rfile.read(length + 2)
+                    chunks.append(chunk[:-2])
+                    if length == 0:
+                        break
+                data = b''.join(chunks)
+                env["CONTENT_LENGTH"] = str(len(data))
+            else:
+                raise AssertionError
         else:
             data = None
             env["CONTENT_LENGTH"] = "0"

commit 0f70b09f02537d1e7b291a6b5b27a29a394213cf

Fix pushing to a repository served by Dulwich itself
This contains a backout/revert of commit e9faf83ac437, which broke
pushing to a Dulwich-served repository using Dulwich itself. The
hg-git testsuite was affected by this; it broke one of our tests.

Fixes #977

dulwich/client.py
@@ -1977,9 +1977,8 @@ class AbstractHttpGitClient(GitClient):
         headers = {
             "Content-Type": "application/x-%s-request" % service,
             "Accept": result_content_type,
+            "Content-Length": str(len(data)),
         }
-        if isinstance(data, bytes):
-            headers["Content-Length"] = str(len(data))
         resp, read = self._http_request(url, headers, data)
         if resp.content_type != result_content_type:
             raise GitProtocolError(
@@ -2041,7 +2040,7 @@ class AbstractHttpGitClient(GitClient):
                 yield from PackChunkGenerator(pack_data_count, pack_data)
 
         resp, read = self._smart_request(
-            "git-receive-pack", url, data=body_generator()
+            "git-receive-pack", url, data=b"".join(body_generator())
         )
         try:
             resp_proto = Protocol(read, None)

dulwich/tests/compat/test_client.py
@@ -602,23 +602,9 @@ class GitHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         try:
             nbytes = int(length)
         except (TypeError, ValueError):
-            nbytes = -1
-        if self.command.lower() == "post":
-            if nbytes > 0:
-                data = self.rfile.read(nbytes)
-            elif self.headers.get('transfer-encoding') == 'chunked':
-                chunks = []
-                while True:
-                    line = self.rfile.readline()
-                    length = int(line.rstrip(), 16)
-                    chunk = self.rfile.read(length + 2)
-                    chunks.append(chunk[:-2])
-                    if length == 0:
-                        break
-                data = b''.join(chunks)
-                env["CONTENT_LENGTH"] = str(len(data))
-            else:
-                raise AssertionError
+            nbytes = 0
+        if self.command.lower() == "post" and nbytes > 0:
+            data = self.rfile.read(nbytes)
         else:
             data = None
             env["CONTENT_LENGTH"] = "0"

dulwich/tests/test_porcelain.py
@@ -33,7 +33,7 @@ import tarfile
 import tempfile
 import threading
 import time
-from unittest import skipIf, expectedFailure
+from unittest import skipIf
 
 from dulwich import porcelain
 from dulwich.diff_tree import tree_changes
@@ -2909,8 +2909,6 @@ class ServerTests(PorcelainTestCase):
         with self._serving() as url:
             porcelain.pull(self.repo, url, "master")
 
-    # TODO: See https://github.com/jelmer/dulwich/issues/977
-    @expectedFailure
     def test_push(self):
         c1, = build_commit_graph(self.repo.object_store, [[1]])
         self.repo.refs[b"refs/heads/master"] = c1.id
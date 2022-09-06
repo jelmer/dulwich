2f2d131e974550382bfa2bff81b83e7705d49146
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-09-06 15:38:23 +0100

    Test is flaky, so skip rather than marking as known failure.

--- dulwich/tests/compat/test_client.py
+++ dulwich/tests/compat/test_client.py
@@ -322,9 +322,6 @@ class DulwichClientTestBase(object):
             for r in result.refs.items():
                 dest.refs.set_if_equals(r[0], None, r[1])
 
-    # This test fails intermittently on my machine, probably due to some sort
-    # of race condition. Probably also related to #1015
-    @expectedFailure
     def test_send_remove_branch(self):
         with repo.Repo(os.path.join(self.gitroot, "dest")) as dest:
             dummy_commit = self.make_dummy_commit(dest)
@@ -431,6 +428,11 @@ class DulwichTCPClientTest(CompatTestCase, DulwichClientTestBase):
         @expectedFailure
         def test_fetch_pack_no_side_band_64k(self):
             DulwichClientTestBase.test_fetch_pack_no_side_band_64k(self)
+
+    def test_send_remove_branch(self):
+        # This test fails intermittently on my machine, probably due to some sort
+        # of race condition. Probably also related to #1015
+        self.skipTest('skip flaky test; see #1015')
 
 
 class TestSSHVendor(object):

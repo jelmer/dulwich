a6335c8b4f0ddd72274864f8e3e5cf7cc9c3929f
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-02-26 21:07:52 +1300

    Update checkout function in procelain.

--- dulwich/porcelain.py
+++ dulwich/porcelain.py
@@ -94,8 +94,8 @@ from dulwich.objectspec import (parse_commit, parse_object, parse_ref,
 from dulwich.pack import write_pack_from_container, write_pack_index
 from dulwich.patch import write_tree_diff
 from dulwich.protocol import ZERO_SHA, Protocol
-from dulwich.refs import (LOCAL_BRANCH_PREFIX, LOCAL_TAG_PREFIX,
-                          _import_remote_refs)
+from dulwich.refs import (LOCAL_BRANCH_PREFIX, LOCAL_REMOTE_PREFIX,
+                          LOCAL_TAG_PREFIX, _import_remote_refs)
 from dulwich.repo import BaseRepo, Repo
 from dulwich.server import (FileSystemBackend, ReceivePackHandler,
                             TCPGitServer, UploadPackHandler)
@@ -123,7 +123,6 @@ class NoneStream(RawIOBase):
 
 default_bytes_out_stream = getattr(sys.stdout, "buffer", None) or NoneStream()
 default_bytes_err_stream = getattr(sys.stderr, "buffer", None) or NoneStream()
-
 
 DEFAULT_ENCODING = "utf-8"
 
@@ -1598,7 +1597,7 @@ def active_branch(repo):
         active_ref = r.refs.follow(b"HEAD")[0][1]
         if not active_ref.startswith(LOCAL_BRANCH_PREFIX):
             raise ValueError(active_ref)
-        return active_ref[len(LOCAL_BRANCH_PREFIX) :]
+        return active_ref[len(LOCAL_BRANCH_PREFIX):]
 
 
 def get_branch_remote(repo):
@@ -1864,18 +1863,18 @@ def checkout(repo, target: bytes, force: bool = False):
     Args:
       repo: dulwich Repo object
       target: branch name or commit sha to checkout
+      force: true or not to force checkout
     """
-
-    # check repo status
+    # Check repo status.
     if not force:
         index = repo.open_index()
         for file in get_tree_changes(repo)['modify']:
             if file in index:
                 raise DirNotCleanError('trying to checkout when working directory not clean')
-        
+
         normalizer = repo.get_blob_normalizer()
         filter_callback = normalizer.checkin_normalize
-        
+
         unstaged_changes = list(get_unstaged_changes(index, repo.path, filter_callback))
         for file in unstaged_changes:
             if file in index:
@@ -1884,19 +1883,30 @@ def checkout(repo, target: bytes, force: bool = False):
     current_tree = parse_tree(repo, repo.head())
     target_tree = parse_tree(repo, target)
 
-    # update head
-    if target == b'HEAD':  # do not update head while trying to checkout to HEAD
-        target = repo.head()
+    # Update head.
+    if target == b'HEAD':  # Do not update head while trying to checkout to HEAD.
+        pass
     elif target in repo.refs.keys(base=LOCAL_BRANCH_PREFIX):
         update_head(repo, target)
-        target = repo.refs[LOCAL_BRANCH_PREFIX + target]
     else:
-        update_head(repo, target, detached=True)
+        # If checking out a remote branch, create a local one without the remote name prefix.
+        config = repo.get_config()
+        name = target.split(b"/")[0]
+        section = (b"remote", name)
+        if config.has_section(section):
+            checkout_target = target.replace(name + b"/", b"")
+            try:
+                branch_create(repo, checkout_target, (LOCAL_REMOTE_PREFIX + target).decode())
+            except Error:
+                pass
+            update_head(repo, LOCAL_BRANCH_PREFIX + checkout_target)
+        else:
+            update_head(repo, target, detached=True)
 
-    # unstage files in the current_tree or target_tree
+    # Un-stage files in the current_tree or target_tree.
     tracked_changes = []
     for change in repo.open_index().changes_from_tree(repo.object_store, target_tree.id):
-        file = change[0][0] or change[0][1]   # no matter the file is added, modified or deleted.
+        file = change[0][0] or change[0][1]  # No matter whether the file is added, modified, or deleted.
         try:
             current_entry = current_tree.lookup_path(repo.object_store.__getitem__, file)
         except KeyError:
@@ -1905,19 +1915,16 @@ def checkout(repo, target: bytes, force: bool = False):
             target_entry = target_tree.lookup_path(repo.object_store.__getitem__, file)
         except KeyError:
             target_entry = None
-        
+
         if current_entry or target_entry:
-            tracked_changes.append(file)
+            tracked_changes.append(file.decode())
+
     repo.unstage(tracked_changes)
 
-    # reset tracked and unstaged file to target
-    normalizer = repo.get_blob_normalizer()
-    filter_callback = normalizer.checkin_normalize
-    unstaged_files = get_unstaged_changes(repo.open_index(), repo.path, filter_callback)
-    for file in unstaged_files:
-        reset_file(repo, file.decode(), b'HEAD')
-    
-    # remove the untracked file which in the current_file_set
+    target_sha = target_tree.sha()
+    repo.reset_index(target_sha.hexdigest())
+
+    # Remove the untracked file which are in the current_file_set.
     for file in get_untracked_paths(repo.path, repo.path, repo.open_index(), exclude_ignored=True):
         try:
             current_tree.lookup_path(repo.object_store.__getitem__, file.encode())

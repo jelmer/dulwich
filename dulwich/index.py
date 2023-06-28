9a732ee05cf8f52353ebfb01ae686758a0e504d2
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2023-06-28 18:56:38 -0400

    add back inadvertantly dropped first line

--- dulwich/index.py
+++ dulwich/index.py
@@ -1,3 +1,4 @@
+# index.py -- File parser/writer for the git index file
 # Copyright (C) 2008-2013 Jelmer Vernooij <jelmer@jelmer.uk>
 #
 # Dulwich is dual-licensed under the Apache License, Version 2.0 and the GNU

2f65054ed85f64b32317baf1f7ceeb9844d0281d
David Lakin <github@themoderndev.com>
2024-08-29 17:48:36 -0400

    Build Fuzz Tests with Rust Extensions and Optional Features
    Updates the OSS-Fuzz container environment & build script to
    build and install Dulwich with all optional features enabled and
    refines dictionary handling.
    
    Impact:
    
    - Increases the fuzz-able surface area of the most interesting
      (from a fuzzing & security testing perspective) Dulwich APIs,
      enabling future fuzz harnesses improvements to specifically
      target these features in tests.
    - Enhances flexibility of fuzzer seed data generation steps to
      improve dictionary quality and simplify seed corpus management
      for all fuzz targets.
    
    Key Changes in `build.sh`:
    - Unset problematic OSS-Fuzz provided `$RUSTFLAGS` to prevent
      build issues that inhibit PyO3 based Rust extension compilation.
    - Install Python dependencies with specific features (`fastimport`,
      `gpg`, `paramiko`, etc.).
    - Added "drop-in" support for inclusion of seed corpora zip files
      & LibFuzzer options files to be introduced later.
    
    Key Changes in `container-environment-bootstrap.sh`:
    - Installs dependencies required to build Dulwich's optional
      features and Rust extensions.
    - Updated Python dependencies (`setuptools`, `pyinstaller`, etc.)
      to the latest compatible versions.
    - Improved dictionary handling for fuzz targets by encapsulating
      dictionary generation logic in shell functions and adding
      default common dictionary entries to all fuzz target specific
      `.dict` files.

--- fuzzing/oss-fuzz-scripts/build.sh
+++ fuzzing/oss-fuzz-scripts/build.sh
@@ -2,36 +2,17 @@
 
 set -euo pipefail
 
-python3 -m pip install .
+unset RUSTFLAGS # The OSS-Fuzz provided RUSTFLAGS cause issues that break PyO3 based Rust extension builds.
+export PATH="${PATH}:${HOME}/.cargo/bin"
+python3 -m pip install -v ".[fastimport,paramiko,https,pgp]"
 
-# Directory to look in for dictionaries, options files, and seed corpora:
-SEED_DATA_DIR="$SRC/seed_data"
-
-find "$SEED_DATA_DIR" \( -name '*_seed_corpus.zip' -o -name '*.options' -o -name '*.dict' \) \
-  ! \( -name '__base.*' \) -exec printf 'Copying: %s\n' {} \; \
+find "$SRC" -maxdepth 1 \
+  \( -name '*_seed_corpus.zip' -o -name '*.options' -o -name '*.dict' \) \
+  -exec printf '[%s] Copying: %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" {} \; \
   -exec chmod a-x {} \; \
   -exec cp {} "$OUT" \;
 
 # Build fuzzers in $OUT.
 find "$SRC/dulwich/fuzzing" -name 'fuzz_*.py' -print0 | while IFS= read -r -d '' fuzz_harness; do
   compile_python_fuzzer "$fuzz_harness"
-
-  common_base_dictionary_filename="$SEED_DATA_DIR/__base.dict"
-  if [[ -r "$common_base_dictionary_filename" ]]; then
-    # Strip the `.py` extension from the filename and replace it with `.dict`.
-    fuzz_harness_dictionary_filename="$(basename "$fuzz_harness" .py).dict"
-    output_file="$OUT/$fuzz_harness_dictionary_filename"
-
-    printf 'Appending %s to %s\n' "$common_base_dictionary_filename" "$output_file"
-    if [[ -s "$output_file" ]]; then
-      # If a dictionary file for this fuzzer already exists and is not empty,
-      # we append a new line to the end of it before appending any new entries.
-      #
-      # LibFuzzer will happily ignore multiple empty lines in a dictionary but fail with an error
-      # if any single line has incorrect syntax (e.g., if we accidentally add two entries to the same line.)
-      # See docs for valid syntax: https://llvm.org/docs/LibFuzzer.html#id32
-      echo >>"$output_file"
-    fi
-    cat "$common_base_dictionary_filename" >>"$output_file"
-  fi
 done

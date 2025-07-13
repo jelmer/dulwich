971a082ad96cff25942041e6d17e927b83d95978
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-07-13 13:53:39 +0100

    Fix typo in license preamble: "public" -> "published"

--- dulwich/contrib/swift.py
+++ dulwich/contrib/swift.py
@@ -5,7 +5,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 # Dulwich is dual-licensed under the Apache License, Version 2.0 and the GNU
-# General Public License as public by the Free Software Foundation; version 2.0
+# General Public License as published by the Free Software Foundation; version 2.0
 # or (at your option) any later version. You can redistribute it and/or
 # modify it under the terms of either of these two licenses.
 #

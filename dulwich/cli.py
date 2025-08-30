f05891827a91f6068aac4fd51f6fc482f514ee6e
xifOO <93408243+xifOO@users.noreply.github.com>
2025-08-31 01:37:08 +0300

    add --no-merged to branch command  (#1874)
    Implements the --no-merged flag for the dulwich branch command, 
    which lists branches that have not been merged into the current branch
    https://github.com/jelmer/dulwich/issues/1847

--- dulwich/cli.py
+++ dulwich/cli.py
@@ -2060,7 +2060,12 @@ class cmd_branch(Command):
         )
         parser.add_argument("--all", action="store_true", help="List all branches")
         parser.add_argument(
-            "--merged", action="store_true", help="List merged branches"
+            "--merged", action="store_true", help="List merged into current branch"
+        )
+        parser.add_argument(
+            "--no-merged",
+            action="store_true",
+            help="List branches not merged into current branch",
         )
         parser.add_argument(
             "--remotes", action="store_true", help="List remotes branches"
@@ -2084,6 +2089,18 @@ class cmd_branch(Command):
         if args.merged:
             try:
                 branches_iter = porcelain.merged_branches(".")
+
+                for branch in branches_iter:
+                    sys.stdout.write(f"{branch.decode()}\n")
+
+                return 0
+            except porcelain.Error as e:
+                sys.stderr.write(f"{e}")
+                return 1
+
+        if args.no_merged:
+            try:
+                branches_iter = porcelain.no_merged_branches(".")
 
                 for branch in branches_iter:
                     sys.stdout.write(f"{branch.decode()}\n")

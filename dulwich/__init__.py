564fa3756979546743eeddec5985e714b3dd2b44
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-10-12 23:27:21 +0100

    Start on 0.24.4

--- dulwich/__init__.py
+++ dulwich/__init__.py
@@ -31,7 +31,7 @@ if sys.version_info >= (3, 10):
 else:
     from typing_extensions import ParamSpec
 
-__version__ = (0, 24, 3)
+__version__ = (0, 24, 4)
 
 __all__ = ["__version__", "replace_me"]
 

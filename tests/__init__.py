e034663eac6ac7279a626f6d126bddd59ec2c178
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-01-06 23:07:07 +0000

    Move porcelain annotate tests to tests/porcelain/

--- tests/__init__.py
+++ tests/__init__.py
@@ -132,9 +132,6 @@ def self_test_suite() -> unittest.TestSuite:
         "bitmap",
         "blackbox",
         "bundle",
-        "cli",
-        "cli_cherry_pick",
-        "cli_merge",
         "client",
         "cloud_gcs",
         "commit_graph",
@@ -197,7 +194,14 @@ def self_test_suite() -> unittest.TestSuite:
         "worktree",
     ]
     module_names = ["tests.test_" + name for name in names]
+    cli_names = [
+        "cherry_pick",
+        "cli",
+        "merge",
+    ]
+    module_names += ["tests.cli.test_" + name for name in cli_names]
     porcelain_names = [
+        "annotate",
         "cherry_pick",
         "filters",
         "lfs",

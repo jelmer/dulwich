e5bc9a4604e1699509447bc09760850062382d8d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-07-05 16:17:48 +0100

    Release 1.2.8

--- dulwich/__init__.py
+++ dulwich/__init__.py
@@ -25,7 +25,7 @@
 from collections.abc import Callable
 from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar
 
-__version__ = (1, 2, 7)
+__version__ = (1, 2, 8)
 
 __all__ = ["__version__", "replace_me"]
 
